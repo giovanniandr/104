10907e12ba53c0b0b654d0c63924751aa8ee08c9
Giovanni Andrade <fs_giovanni@hotmail.com>
2022-11-12 16:01:26 +0000

    Tabs being working on

--- front/pages/components/Content.js
+++ front/pages/components/Content.js
@@ -2,18 +2,13 @@ import React from 'react'
 
 function Content() {
   return (
-    <main className="float-left p-6 bg-red-100 sm:p-10">
-      <div className="float-left mr-6">
-          <h1> Menu </h1>
-        </div>
-      <div class="bg-[#F5F5F5] flex space-x-5 md:space-x-0 md:flex-row justify-between">
-        
-        <button>
-          Breakfast
-        </button>
+    <main className="">
+      <div className="p-6">
+        <h1> Menu </h1>
       </div>
+
     </main>
-  )
+  );
 }
 
 export default Content
