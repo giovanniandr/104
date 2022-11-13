7b31643a04721e7c54f5d11e099a20f62d6ad54a
Giovanni Andrade <fs_giovanni@hotmail.com>
2022-11-13 16:04:11 +0000

    Update dom properties

--- front/pages/components/Login.js
+++ front/pages/components/Login.js
@@ -53,23 +53,23 @@ function Login() {
       </div>
       <div className="w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12 flex items-center justify-center bg-[#FDFCFA]">
         <div className="w-full h-100">
-          <h1 className="mt-12 text-xl font-medium leading-tight md:text-2xl"> Welcome to your restaurant manangement </h1>
+          <h1 className="mt-12 text-xl font-medium leading-tight md:text-2xl"> Welcome to Restaurant Manangement </h1>
           <form class="mt-6" action="#" method="POST">
         <div>
-          <label class="block text-gray-700">Email Address</label>
-          <input type="email" name="" id="" placeholder="Enter email address" class="w-full px-4 py-3 rounded-lg  mt-2 border bg-[#F5F5F5] focus:bg-[#FFFFFF] border-none focus:outline-none" autofocus autocomplete required/>
+          <label className="block text-gray-700">Email Address</label>
+          <input type="email" name="" id="" placeholder="Enter email address" className="w-full px-4 py-3 rounded-lg  mt-2 border bg-[#F5F5F5] focus:bg-[#FFFFFF] border-none focus:outline-none" autofocus autocomplete required/>
         </div>
 
-        <div class="mt-4">
-          <label class="block text-gray-700">Password</label>
-          <input type="password" name="" id="" placeholder="Enter password" minlength="6" class="w-full px-4 py-3 rounded-lg  mt-2 border bg-[#F5F5F5] focus:bg-[#FFFFFF] border-none focus:outline-none" required/>
+        <div className="mt-4">
+          <label className="block text-gray-700">Password</label>
+          <input type="password" name="" id="" placeholder="Enter password" minlength="6" className="w-full px-4 py-3 rounded-lg  mt-2 border bg-[#F5F5F5] focus:bg-[#FFFFFF] border-none focus:outline-none" required/>
         </div>
 
-        <div class="text-right mt-2">
-          <a href="#" class="text-sm font-semibold text-[#363C3C] hover:opacity-90 focus:opacity-80">Forgot the password ?</a>
+        <div className="text-right mt-2">
+          <a href="#" className="text-sm font-semibold text-[#363C3C] hover:opacity-90 focus:opacity-80">Forgot the password ?</a>
         </div>
 
-        <button type="submit" class="w-full block bg-[#363C3C] hover:opacity-90 focus:opacity-80 text-white font-medium rounded-lg
+        <button type="submit" className="w-full block bg-[#363C3C] hover:opacity-90 focus:opacity-80 text-white font-medium rounded-lg
               px-4 py-3 mt-6">Log In</button>
       </form>
         </div>
