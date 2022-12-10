323d571689f6df3014db4650ea9781328b6545b6
Giovanni Andrade <fs_giovanni@hotmail.com>
2022-12-10 22:09:33 +0000

    Grid update

--- front/pages/index.js
+++ front/pages/index.js
@@ -4,23 +4,16 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="h-screen bg-blue-200 p-16 grid grid-cols-4  grid-rows-4 gap-6">
+    <div className="h-screen bg-blue-200 p-1 grid grid-cols-4  grid-rows-4 gap-1">
         <title>Restaurant 104 Management</title>
         <meta name="description" content="Software as a Service by Beag."/>
         <link rel="icon" href="/favicon.ico" />
 
-    <div className="bg-blue-500 text-white text-center text-5xl py-4 rounded-lg"> 1 </div>
-    <div className="bg-blue-500 text-white text-center text-5xl py-4 rounded-lg"> 2 </div>
-    <div className="bg-blue-500 text-white text-center text-5xl py-4 rounded-lg"> 3 </div>
-    <div className="bg-blue-500 text-white text-center text-5xl py-4 rounded-lg"> 4 </div>
-    <div className="bg-blue-500 text-white text-center text-5xl py-4 rounded-lg"> 5 </div>
-    <div className="bg-blue-500 text-white text-center text-5xl py-4 rounded-lg"> 6 </div>
-    <div className="bg-blue-500 text-white text-center text-5xl py-4 rounded-lg"> 7 </div>
-    <div className="bg-blue-500 text-white text-center text-5xl py-4 rounded-lg"> 8 </div>
-    <div className="bg-blue-500 text-white text-center text-5xl py-4 rounded-lg"> 9 </div>
-    <div className="bg-blue-500 text-white text-center text-5xl py-4 rounded-lg"> 10 </div>
-    <div className="bg-blue-500 text-white text-center text-5xl py-4 rounded-lg"> 11 </div>
-    <div className="bg-blue-500 text-white text-center text-5xl py-4 rounded-lg"> 12 </div>
+    <div className="bg-blue-500 text-white text-center text-5xl py-4 rounded-lg row-span-4 min-sm-w-2/5 max-lg-w-2/4 w-2/4"> 1 </div>
+    <div className="bg-blue-500 text-white text-center text-5xl py-4 rounded-lg col-span-3"> 2 </div>
+    <div className="bg-blue-500 text-white text-center text-5xl py-4 rounded-lg row-span-3 col-span-3"> 3 </div>
+
+    
   
     </div>
   )
