92e17f03b91d9a1eb23880f78cc300448a56da59
Giovanni Andrade <fs_giovanni@hotmail.com>
2022-12-10 22:23:07 +0000

    Grid update

--- front/pages/index.js
+++ front/pages/index.js
@@ -4,15 +4,13 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="h-screen bg-blue-200 p-1 grid grid-cols-4  grid-rows-4 gap-1">
+    <div className="h-screen bg-blue-200 grid grid-cols-2 grid-row-0 gap-1">
         <title>Restaurant 104 Management</title>
         <meta name="description" content="Software as a Service by Beag."/>
         <link rel="icon" href="/favicon.ico" />
 
-    <div className="bg-blue-500 text-white text-center text-5xl py-4 rounded-lg row-span-4 min-sm-w-2/5 max-lg-w-2/4 w-2/4"> 1 </div>
-    <div className="bg-blue-500 text-white text-center text-5xl py-4 rounded-lg col-span-3"> 2 </div>
-    <div className="bg-blue-500 text-white text-center text-5xl py-4 rounded-lg row-span-3 col-span-3"> 3 </div>
-
+    <div className="bg-blue-500 text-white text-center text-5xl row-span-4 sm-w-1/4 md-w-2/5 lg-w-2/5 xl-w-2/5 w-2/5"> 1 </div>
+    <div className="bg-blue-500 text-white text-center text-5xl col-span-3 row-span-4 w-full"> 2 </div>
     
   
     </div>
