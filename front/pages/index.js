3e79f02e437c2d754bc78d85bb59591ef23a20ed
Giovanni Andrade <fs_giovanni@hotmail.com>
2022-12-10 22:55:15 +0000

    Update layout

--- front/pages/index.js
+++ front/pages/index.js
@@ -4,13 +4,13 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="h-screen bg-blue-200 grid grid-cols-2 grid-row-0 gap-1">
+    <div className="h-screen bg-blue-200 grid grid-cols-12 grid-rows-1 gap-1">
         <title>Restaurant 104 Management</title>
         <meta name="description" content="Software as a Service by Beag."/>
         <link rel="icon" href="/favicon.ico" />
 
-    <div className="bg-blue-500 text-white text-center text-5xl row-span-4 sm-w-1/4 md-w-2/5 lg-w-2/5 xl-w-2/5 w-2/5"> 1 </div>
-    <div className="bg-blue-500 text-white text-center text-5xl col-span-3 row-span-4 w-full"> 2 </div>
+    <div className="bg-blue-500 text-white text-center text-5xl"> <Navbar /></div>
+    <div className="bg-blue-500 text-white text-center text-5xl col-span-11"> <Content /> </div>
     
   
     </div>
